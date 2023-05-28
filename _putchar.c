88af41eaa080bcea0db01a6617a05c1eb131a3b5
yaalipsy <yaalipsy@github.com>
2023-05-28 14:58:01 -0700

    update

--- _putchar.c
+++ _putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * putchar - prints out the character c
+ * _putchar - prints out the character c
  * @c: the character to be printed
  * Return: 1 on sucess and -1 if otherwise
  */
 
-int putchar(char)
+int _putchar(char)
 {
 	return (write(1, &c, 1));
 }
