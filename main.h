133190462944e19f68e72f0bc563fc508f6af105
victoria <128818060+Adwoavicky@users.noreply.github.com>
2023-05-30 05:04:52 +0000

    Update main.h

--- main.h
+++ main.h
@@ -37,7 +37,7 @@ struct fmt
 * @fmt_t: the function associated
 */
 
-typedef struct fmt fmr_t
+typedef struct fmt fmt_t
 {
 	int _printf(const char *format, ...);
 	int handle_print(const char *fmt, int *i,
